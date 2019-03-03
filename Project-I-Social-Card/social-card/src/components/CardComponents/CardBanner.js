fa44857b7f600d9985e08456f585d92a525916ee
Antony Nyagah <antonynyagah@antonys-MBP.attlocal.net>
2019-03-02 19:57:25 -0500

    initial commit for card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+i
+function CardBanner() {
+    return <img className="banner" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"
+    alt="React logo banner"
+    />
+}
+
+export default CardBanner;
